bf923700050091bc4ad19798a3de38ae63a4abb9
viknesh2952 <smileyviknesh007@gmail.com>
2021-01-26 16:48:12 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component, VERSION } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import $ from "jquery";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  name = "Angular " + VERSION.major;
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    $(document).ready(() => {
+      $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+          $(".nav").addClass("affix");
+        } else {
+          $(".nav").removeClass("affix");
+        }
+      });
+      $(".navTrigger").click(function() {
+        $(this).toggleClass("active");
+        $("#mainListDiv").toggleClass("show_list");
+        $("#mainListDiv").fadeIn();
+      });
+    });
+  }
 }
