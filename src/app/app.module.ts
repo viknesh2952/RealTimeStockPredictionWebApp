fa9095e497ad3157f55df401b7805554f7748f66
viknesh2952 <smileyviknesh007@gmail.com>
2021-02-10 15:41:41 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,15 @@ import { AppRoutingModule } from "./app-routing.module";
 import { NotfoundComponent } from "./notfound/notfound.component";
 import { HomeComponent } from "./home/home.component";
 import { StocksComponent } from "./stocks/stocks.component";
-import { HighchartsChartComponent } from "highcharts-angular";
+
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import stock from 'highcharts/modules/stock.src';
+import more from 'highcharts/highcharts-more.src';
+
+export function highchartsModules() {
+  // apply Highcharts Modules to this array
+  return [stock, more];
+}
 @NgModule({
   imports: [
     BrowserModule,
@@ -20,17 +28,17 @@ import { HighchartsChartComponent } from "highcharts-angular";
     FormsModule,
     NgbModule,
     AgGridModule.withComponents([]),
-    HttpClientModule
+    HttpClientModule,ChartModule
   ],
   declarations: [
     AppComponent,
     HelloComponent,
     NotfoundComponent,
     HomeComponent,
-    StocksComponent,
-    // HighchartsChartComponent
+    StocksComponent
   ],
   bootstrap: [AppComponent],
-  providers: [ApiService]
+  providers: [ApiService,
+    { provide: HIGHCHARTS_MODULES, useFactory: highchartsModules }]
 })
 export class AppModule {}
