87e1115dc8f71ec1274159bbb967883fc72a4bc0
viknesh2952 <smileyviknesh007@gmail.com>
2021-02-20 19:35:54 +0530

    2 files modified

--- src/app/stocks/stocks.component.ts
+++ src/app/stocks/stocks.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { ApiService } from "../api.service";
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import { MenuModule } from "@ag-grid-enterprise/menu";
 import { ColumnsToolPanelModule } from "@ag-grid-enterprise/column-tool-panel";
 import * as Highcharts from "highcharts/highstock";
+import { AgGridAngular } from "ag-grid-angular";
 @Component({
   selector: "app-stocks",
   templateUrl: "./stocks.component.html",
   styleUrls: ["./stocks.component.css"]
 })
 export class StocksComponent implements OnInit {
+  @ViewChild("agGrid") agGrid: AgGridAngular;
   updateFlag = false;
   data = [];
   rowData;
+  enableplot = false;
   table = "Exchanges";
   gridOptions = {
     columnDefs: "columnDefs",
@@ -65,9 +68,13 @@ export class StocksComponent implements OnInit {
       }
     ]
   };
-  handleUpdate() {
-    this.api.getChart("ALEAF", "1day").subscribe((data: any) => {
+  handleUpdate(stock, interval) {
+    this.api.getChart(stock, interval).subscribe((data: any) => {
       console.log(data);
+      if (data.status == "error") {
+        alert("This is not available.Please, select something!!!");
+        return;
+      }
       this.chartOptions.title = {
         text:
           data.meta.symbol + " Stocks  - " + data.meta.exchange + " Exchange"
@@ -78,17 +85,53 @@ export class StocksComponent implements OnInit {
       for (let i = 0; i < data.values.length; i++) {
         this.data.push(Number(data.values[i].close));
       }
+      var l = data.values.length - 1;
+      var date = data.values[l].datetime.split("-");
+      date[1] = date[1] - 1;
+      /////////
+      var mod = [];
+      var d = date[1] + 1;
+      for (let i = 0; i < data.values.length; i++) {
+        switch (d) {
+          case 1:
+            console.log("January");
+          case 2:
+            console.log("February");
+          case 3:
+
+          case 4:
+
+          case 5:
+
+          case 6:
+
+          case 7:
+
+          case 8:
+
+          case 9:
+
+          case 10:
+
+          case 11:
+
+          case 12:
+        }
+        mod.push(Number(data.values[i].close));
+      }
+      /////////
       setTimeout(() => {
         this.chartOptions.series[0] = {
           type: "area",
-          data: this.data
+          data: this.data,
+          pointStart: Date.UTC(date[0], date[1], date[2])
         };
         this.updateFlag = true;
       }, 500);
     });
   }
   ngOnInit() {
-    this.handleUpdate();
+    this.handleUpdate("ALEAF", "1day");
     this.columnDefs = [
       { field: "code" },
       { field: "country" },
@@ -100,9 +143,25 @@ export class StocksComponent implements OnInit {
       this.rowData = data.data;
     });
   }
+  Plot() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    var selectedId = selectedNodes.map(node => node.data.symbol);
+    if (selectedId == undefined || selectedId.length == 0) {
+      alert("Please, select any one stock!!!");
+    } else {
+      this.handleUpdate(selectedId, "1day");
+    }
+    console.log(selectedId);
+  }
   getStocks() {
     this.columnDefs = [];
     this.columnDefs = [
+      // {
+      //   field: "action",
+      //   cellRenderer: data => {
+      //     return `<button (click)="Plot()">btn</button>`;
+      //   }
+      // },
       { field: "country" },
       { field: "currency" },
       { field: "exchange" },
@@ -110,6 +169,7 @@ export class StocksComponent implements OnInit {
       { field: "symbol" },
       { field: "type" }
     ];
+    this.enableplot = true;
     this.api.getStocksList().subscribe((data: any) => {
       console.log(data);
       this.rowData = data.data;
@@ -117,6 +177,7 @@ export class StocksComponent implements OnInit {
     this.table = "Stocks";
   }
   getExchanges() {
+    this.enableplot = false;
     this.columnDefs = [];
     this.columnDefs = [
       { field: "code" },
@@ -131,6 +192,7 @@ export class StocksComponent implements OnInit {
     this.table = "Exchanges";
   }
   getIndices() {
+    this.enableplot = false;
     this.columnDefs = [];
     this.columnDefs = [
       { field: "country" },
